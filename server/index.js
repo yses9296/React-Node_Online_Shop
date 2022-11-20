f684a8ce060e1ea0a8bdea8df925df606c56e07c
Eunseo Choi <yses9296@gmail.com>
2022-11-20 23:06:03 +0900

    Node-React init

--- server/index.js
+++ server/index.js
@@ -1,116 +1,54 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require("express");
+const app = express();
+const path = require("path");
+const cors = require('cors')
 
-const config = require('./config/key');
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const config = require("./config/key");
 
-//application/x-www-form-urlencoded 분석
-app.use(bodyParser.urlencoded({extended: true})); 
+const mongoose = require("mongoose");
+const connect = mongoose.connect(config.mongoURI,
+  {
+    useNewUrlParser: true, useUnifiedTopology: true,
+    useCreateIndex: true, useFindAndModify: false
+  })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
 
-//application/json 분석
-app.use(bodyParser.json()); 
+app.use(cors())
+
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
+app.use(bodyParser.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
+app.use(bodyParser.json());
 app.use(cookieParser());
 
-//mongoDB connection - mongoose
-const mongoose = require('mongoose');
-mongoose.connect(config.mongoURI)
-.then( ()=> console.log('MongoDB Connected..')) //MongoDB Connection Succeed
-.catch( err => console.log(err))                //MongoDB Connection Failed
+app.use('/api/users', require('./routes/users'));
 
 
-//test init
-app.get('/', (req, res) => {
-  res.send('Hello World! This is Node with React')
-})
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
+app.use('/uploads', express.static('uploads'));
 
-//React JS + Node
-app.get('/api/hello',(req, res) => {
-  res.send('Hello You reached Node js');
-})
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
 
+  // Set static folder   
+  // All the javascript and css files will be read and served from this folder
+  app.use(express.static("client/build"));
 
-
-//Register Route
-const { User } = require('./models/User');
-
-app.post('/api/users/register', (req, res) => {//회원가입에 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다.
-  const user = new User(req.body); //인스턴스 생성
-  user.save((err, userInfo) => {
-    if(err) return res.json( {success: false, err})
-    return res.status(200).json({
-      success: true
-    })
+  // index.html for all page routes    html or routing and naviagtion
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
-})//Register Route
+}
 
-
-//Login Route
-app.post('/api/users/login', (req, res) => {
- 
-  //step 1: 요청된 이메일을 데이터베이스에서 찾는다.
-  User.findOne({ email: req.body.email} , (err, userInfo) => {
-
-    if(!userInfo) {
-      return res.json({loginSuccess: false, message: "존재하지 않는 이메일입니다."})
-    }
-
-    //step 2: 요청한 email가 존재한다면 비밀번호가 같은지 확인
-    userInfo.comparePassword(req.body.password, (err, isMatch) => {
-      if(!isMatch)
-        return res.json({loginSuccess: false, message: "비밀번호가 일치하지 않습니다." })
-      //step 3: 이메일, 비밀번호 모두 일치 시 토큰 생성
-      userInfo.generateToken((err, user) => {
-        if(err) return res.status(400).send(err);
-
-        //토큰을 저장한다 (쿠키 또는 로컬스토리지에 저장 가능)
-        res.cookie("x_auth", user.token)
-        .status(200)
-        .json({loginSuccess: true, userId: user._id})
-      })
-
-      
-    })
-  })
-});//Login-Route
-
-
-//Login - Authentication
-const { auth } = require('./middleware/auth');
-app.get('/api/users/auth', auth, (req, res) => {
-  console.log('through middleware')
-
-  res.status(200).json({
-    _id: req.user._id,
-    isAdmin: req.user.role === 0 ? false : true,
-    isAuth: true,
-    email: req.user.email,
-    name: req.user.name,
-    lastname: req.user.lastname,
-    role: req.user.role,
-    image: req.user.image,
-  })
-
-})//Login - Authentication
-
-
-
-//Logout - Authentication
-app.get('/api/users/logout', auth, (req, res) => {
-  User.findOneAndUpdate(
-    {_id: req.user._id}, 
-    {token: ''}, 
-    (err, user) => {
-      if(err) return res.json({success: false, err})
-      return res.status(200).send({success: true})
-    }
-  )
-})//Logout - Authentication
-
-
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Server Listening on ${port}`)
+});
