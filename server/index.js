6f77a09802e69b2ee2c73aa48c0552542df628c0
Eunseo Choi <yses9296@gmail.com>
2022-11-20 23:07:03 +0900

    Node-React init-server

--- server/index.js
+++ server/index.js
@@ -19,29 +19,19 @@ const connect = mongoose.connect(config.mongoURI,
 
 app.use(cors())
 
-//to not get any deprecation warning or error
-//support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
-//to get json data
-// support parsing of application/json type post data
+
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use('/api/users', require('./routes/users'));
 
-
-//use this to show the image you have in node js server to client (react js)
-//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use('/uploads', express.static('uploads'));
 
-// Serve static assets if in production
+
+
 if (process.env.NODE_ENV === "production") {
-
-  // Set static folder   
-  // All the javascript and css files will be read and served from this folder
   app.use(express.static("client/build"));
-
-  // index.html for all page routes    html or routing and naviagtion
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
