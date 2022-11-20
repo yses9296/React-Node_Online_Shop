f4a680914a7137b46927f7561afc35ee8f097469
Eunseo Choi <yses9296@gmail.com>
2022-11-20 23:08:53 +0900

    Node-React init-server

--- server/index.js
+++ server/index.js
@@ -9,11 +9,8 @@ const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 const mongoose = require("mongoose");
-const connect = mongoose.connect(config.mongoURI,
-  {
-    useNewUrlParser: true, useUnifiedTopology: true,
-    useCreateIndex: true, useFindAndModify: false
-  })
+const connect = mongoose.connect(config.mongoURI) 
+// ,{useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true, useFindAndModify: false})
   .then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
